aa246faccf586c3b8b7b659edb87c0f6ddafe23b
talles-alexandre <alexandretalles13@gmail.com>
2022-12-12 01:23:14 -0300

    create route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import categoryRouter from "./routes/categoryRouter.js";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(json());
 
-const PORT = process.env.Port || 5000;
-app.listen(PORT, () => console.log("server running"));
+app.use(categoryRouter);
+
+app.listen(4000, () => console.log("server running"));
